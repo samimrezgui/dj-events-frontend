2463729a865cec0c872efec295a7052b05bbeaff
sami mrezgui <samimrezgui123@gmail.com>
2023-02-13 14:05:14 +0100

    add new event

--- pages/events/add.js
+++ pages/events/add.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function AddEventPage() {
   return (
     <div>
-      <h1>Add event</h1>
+      <h1>Add New Event</h1>
+      <h1>Adding event</h1>
     </div>
   );
 }
