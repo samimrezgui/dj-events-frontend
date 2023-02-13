3ba47d64ecbe608de927a38a6efc4003f3be6431
sami mrezgui <samimrezgui123@gmail.com>
2023-02-13 16:15:12 +0100

    Pages and Routing

--- pages/events/add.js
+++ pages/events/add.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function AddEventPage() {
   return (
     <div>
-      <h1>Add New Event</h1>
-      <h1>Adding event</h1>
+      <h1>Add event</h1>
     </div>
   );
 }
